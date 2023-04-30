e1c09ffe2158c7dc284815fd4b933e0b3ec07e3a
jorisdandii <jorisdandii@gmail.com>
2023-04-30 20:12:14 +0900

    test done

---
+++ index.js
@@ -0,0 +1,17 @@
+//  Module package CORE
+
+// let number = 0;
+// setInterval(function () {
+//   console.log("hisob:", number);
+//   number++;
+// }, 0);
+
+
+const fs = require('fs');
+const data = fs.readFileSync('./input.txt', 'utf8');
+console.log(data);
+
+
+
+//  Module package EXTERNAL
+//  Module package FILE
